680e33afb3664c3a61f64f3cca9167a15a36364c
王 峰 <_ReacTor@oumatoair.wireless.dartmouth.edu>
2016-01-01 02:04:17 -0500

    add preorder traversal and some notes

--- Comparsion Sort/binaryTree.js
+++ Comparsion Sort/binaryTree.js
@@ -1,9 +1,10 @@
-
 /*************** Binary Search Tree ******************
 reference: http://wuzhiwei.net/ds_app_bst/
+		   http://khan4019.github.io/front-end-Interview-Questions/bst.html
 */
 function BinarySearchTree(){
 	this.root = null;
+	this.height = 0;
 }
 
 function BstNode(key, value, parent){
@@ -86,57 +87,79 @@ BinarySearchTree.prototype._remove = function(key, value, node){
 	}
 	
 	if(node.valueList.length > 1){
-		node.valueList.splice(node.valueList.indexOf(value), 1);
+		var index = node.valueList.indexOf(value);
+		node.valueList.splice(index ? index : 0, 1);
 		return;
 	}else if(node.left && node.right){
 		var rightMin = this._findMin(node.right);
-		if(node == node.parent.left){
-			node.parent.left.key = rightMin.key;
-			node.parent.left.valueList = rightMin.valueList;
-		}else{
-			node.parent.right.key = rightMin.key;
-			node.parent.right.valueList = rightMin.valueList;
-		}
 		
+		node.key = rightMin.key;
+		node.valueList = rightMin.valueList;
+		
+		//After we copy the rightMin parameter to the toRemoved BstNode, the BST is broken
+		//actually now there're two nodes == rightMin, and by using _find() function, we can only get the first one
+		//what we want and need to remove is the second(original) rightMin, so the third parament is introduced
+		//it directly set the node to be removed and skip the _find() process
 		this._remove(key, value, rightMin);
 	}else if(!node.left && !node.right){
 		(node == node.parent.left)? node.parent.left = null : node.parent.right = null;
 		return ;
 	}else{
-		if(node.left){
-			node.parent.left = node.left;
-		}else{
-			node.parent.right = node.right;
-		}
-		
+		node.left ? node.parent.left = node.left : node.parent.right = node.right;
 		return;
 	}
 };
 
-BinarySearchTree.prototype._inOrderTravel = function(){
+// The order is Left / Node / Right
+BinarySearchTree.prototype._inorderTraversal = function(func){
 	var node = this.root;
 	var stack = [];
 
-	var orderedArr = [];
 
 	while(node){
 		if(node.left){
 			stack.push(node);
 			node = node.left;
 		}else{
-			orderedArr.push(node);
+			func(node);
 			while(!node.right){
 				node = stack.pop();
 				if(!node){
-					return orderedArr;
+					return ;
 				}
-				orderedArr.push(node);
+				func(node);
 			}
 			node = node.right;
 		}
 	}
 
 	return orderedArr;
+};
+
+// The order is Node / Left / Right
+BinarySearchTree.prototype._preorderTraversal = function(func){
+	var node = this.root;
+	var stack = [];
+
+	while(node){
+		func(node);
+		if(node.left){
+			if(stack[stack.length-1]!=node){
+				stack.push(node);
+			}
+			node = node.left;
+		}else{
+			while(!node.right){
+				node = stack.pop();
+				if(!node){
+					return;
+				}
+			}
+
+			node = node.right;
+			stack.push(node);
+		}
+	}
 };
 
 
@@ -166,49 +189,82 @@ function testCorrect(arr){
 	return true;
 };
 
-/* By using Binary Search Tree, we can easily get an sorted arr from the input.
-just built a BST and then do inorder travel
+
+/****************** sorting test **********************
+ By using Binary Search Tree, we can easily get an sorted arr from the input.
+just built a BST and then do inorder travel, here's an example
 */
-/*initialArr();
+initialArr();
 
 var BST = new BinarySearchTree();
 for(var i = 0; i<arrLen; i++){
 	BST._add(arr[i], i);
 }
 
-var orderedElements = BST._inOrderTravel();
-var sortedArr = [];
+function outputSortedArr(BST){
+	var orderedElements = [];
+	BST._inorderTraversal(function(node){
+		orderedElements.push(node);
+	});
+	var sortedArr = [];
 
-for(var j = 0; j < orderedElements.length; j++){
-	for(var k = 0; k<orderedElements[j].valueList.length; k++){
-		sortedArr.push(orderedElements[j].key);
+	for(var j = 0; j < orderedElements.length; j++){
+		for(var k = 0; k<orderedElements[j].valueList.length; k++){
+			sortedArr.push(orderedElements[j].key);
+		}
 	}
+
+	return sortedArr;
 }
 
-console.log("BinarySearchTree Sort", testCorrect(sortedArr), sortedArr);*/
+var result = outputSortedArr(BST);
+console.log("BinarySearchTree Sort", testCorrect(result), result);
 
-/*remove*/
+
+
+/****************** preorder traversal test *************
+*/
+var preorderArr = [40, 25, 78, 10, 32, 7, 31, 35];
+var preorderBST = new BinarySearchTree();
+
+for(var i = 0; i<preorderArr.length; i++){
+	preorderBST._add(preorderArr[i], i);
+}
+
+var preorderElements = [];
+preorderBST._preorderTraversal(function(node){
+	preorderElements.push(node);
+});
+
+console.log(preorderElements);
+
+
+
+/****************** remove test **********************
+_remove(key, value, node)
+we only use two parameters
+*/
+
+var toRemove = 11;
+//because this BST accept the same key, their difference(value) stored in BstNode.valueList
+//So when we try to remove a key with specific value, we should provide the value, or it'll
+//del the first value in the valueList
+var toRemoveValue; 
+
 var testBST = new BinarySearchTree();
-var testArr = [15,11,17,8,12,16,18,7,10,13,9];
+var testArr = [15,11,17,8,12,16,18,7,10,13,9,8];
 for(var i = 0; i<testArr.length; i++){
 	testBST._add(testArr[i], i);
 }
 
+testBST._remove(toRemove, toRemoveValue);
 
-var toRemove = 11;
+//After removal, we can check the correctness of the remaining BST by checking the correctness
+//of the sorted array
 
-testBST._remove(toRemove);
+var result = outputSortedArr(testBST);
 
-var orderedElements = testBST._inOrderTravel();
-var sortedArr = [];
-
-for(var j = 0; j < orderedElements.length; j++){
-	for(var k = 0; k<orderedElements[j].valueList.length; k++){
-		sortedArr.push(orderedElements[j].key);
-	}
-}
-
-console.log(sortedArr);
+console.log(result, testCorrect(result));
 
 
 
